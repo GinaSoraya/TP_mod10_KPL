625b89ff09ce9b2eaec212c5f9b7284664961924
GinaSoraya <ginasry08@gmail.com>
2024-04-28 21:53:14 +0700

    menambahkan API mahasiswa

---
+++ tpmodul10_1302223070/Controllers/MahasiswaController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace tpmodul10_1302223070.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MahasiswaController : ControllerBase
+    {
+        private static List<Mahasiswa> dataMahasiswa = new List<Mahasiswa>
+        {
+            new Mahasiswa("Gina Soraya", "1302223070"),
+            new Mahasiswa("Hafid Alakhyar", "1302223044"),
+            new Mahasiswa("Ragadhitya Janatantra Koeshutama", "1302223025"),
+            new Mahasiswa("Muhammad Iqbal Nurhaq", "1302223050"),
+            new Mahasiswa("Kal El Muhammad Pratama", "1302220096")
+        };
+        // GET: api/<ValuesController>
+        [HttpGet]
+        public IEnumerable<Mahasiswa> Get()
+        {
+            return dataMahasiswa;
+        }
+
+        // GET api/<ValuesController>/5
+        [HttpGet("{id}")]
+        public Mahasiswa Get(int id)
+        {
+            return dataMahasiswa[id];
+        }
+
+        // POST api/<ValuesController>
+        [HttpPost]
+        public void Post([FromBody] Mahasiswa value)
+        {
+            dataMahasiswa.Add(value);
+        }
+
+        // DELETE api/<ValuesController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            dataMahasiswa.RemoveAt(id);
+        }
+    }
+}
--- tpmodul10_1302223070/obj/Debug/net8.0/tpmodul10_1302223070.AssemblyInfo.cs
+++ tpmodul10_1302223070/obj/Debug/net8.0/tpmodul10_1302223070.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("tpmodul10_1302223070")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f535df23514f64e4f083cccab9896866a198be08")]
 [assembly: System.Reflection.AssemblyProductAttribute("tpmodul10_1302223070")]
 [assembly: System.Reflection.AssemblyTitleAttribute("tpmodul10_1302223070")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
